3a7f9c0314ff3bfc598d158feef237dd78162a4f
kriskowal <kris.kowal@cixar.com>
2009-10-07 18:05:37 -0700

    Improvements on the Mac

--- examples/fw/Jakefile.js
+++ examples/fw/Jakefile.js
@@ -3,23 +3,17 @@ var FILE = require("file");
 
 use("jake/c");
 
-if (system.os == "Mac OS X") {
-    exports.cFlags = [
-        '-Wall',
-        '-O3',
-        '-I/Developer/SDKs/MacOSX10.5.sdk/usr/include',
-        '-L/Developer/SDKs/MacOSX10.5.sdk/usr/lib'
-    ];
-} else {
-    exports.cFlags = [
-        '-ansi',
-        '-pedantic',
-        '-O3',
+exports.cFlags = [
+    '-ansi',
+    '-pedantic',
+    '-O3',
+    '-Wall'
+];
+if (system.os != "Mac OS X") {
+    exports.cFlags.push(
         '-static',
-        '-static-libgcc',
-        '-Wall',
-        '-O3'
-    ];
+        '-static-libgcc'
+    );
 }
 
 task("default", "fw");
